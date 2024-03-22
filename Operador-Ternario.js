2bdd80a6bade1c27d3018c06432e285ba3556a8a
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2024-03-22 11:00:43 -0300

    JavaScript

---
+++ Operador-Ternario.js
@@ -0,0 +1,11 @@
+//Operador Ternario -->   teste/condição ? true : false
+
+const pontuacao = 500;
+
+//se a condição for true exibi usuario vip se for false exibi usuario normal
+                // teste/condição   //se for true   //se for false
+const usuario = pontuacao >= 1000 ? 'usuario vip' : 'usuario normal';
+
+console.log(usuario);
+
+//se a condição for true exibi usuario vip se for false exibi usuario normal
