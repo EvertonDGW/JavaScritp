ea4db311293844e26c0cb6ccad8d7f4c1437ff4d
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2024-04-06 21:30:11 -0300

    javascript

--- Atribuição-Desestruturação-Objeto.js
+++ Atribuição-Desestruturação-Objeto.js
@@ -13,5 +13,7 @@ const pessoa = { //criamos um objeto
 
 //vamos usar a desestruturação via atribuição para OBJETOS
 
-const { nome : n, sobrenome, idade } = pessoa; // quando usamos nome : n. estamos fazendo com que os valores da variavel nome sejam passados para dentro da variavel n
-console.log(n,sobrenome)
+// quando usamos nome : n. estamos fazendo com que os valores da variavel nome sejam passados para dentro da variavel n
+// quando usamos endereco:{rua,numero}. estamos pengado valores especificos dentro de endereco, ou seja estamos pegando o valor da rua e numero
+const { nome : n, sobrenome, idade, endereco:{rua,numero}, endereco} = pessoa; 
+console.log(n,sobrenome,idade,rua,numero,endereco);
