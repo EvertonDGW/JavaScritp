97f03f26e3fe86587db1c823446005d03b2983b2
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2024-05-30 16:45:19 -0300

    javascript

---
+++ core-js-e-regenerator-runtime.js
@@ -0,0 +1,3 @@
+/* core-js e regenerator-runtime são bibliotecas utilizadas para garantir compatibilidade de código JavaScript moderno 
+(ES6 e posterior) em ambientes que não suportam totalmente esses recursos. Elas são especialmente úteis em projetos que precisam 
+funcionar em uma ampla variedade de navegadores e versões do Node.js. */
---
+++ módulos-ES6.js
@@ -0,0 +1,8 @@
+/* Os módulos ES6 (também conhecidos como ECMAScript 2015 Modules ou JavaScript Modules) são uma maneira padronizada de organizar e compartilhar código JavaScript entre diferentes arquivos e partes de um aplicativo. A introdução dos módulos ES6 trouxe uma maneira nativa e mais robusta de modularizar o código, facilitando a manutenção, reutilização e organização.
+
+Principais Conceitos dos Módulos ES6
+Exportação:
+Você pode exportar variáveis, funções ou classes de um módulo para torná-los disponíveis para outros módulos. Existem duas formas principais de exportação: nomeada e padrão.
+
+Importação:
+Você pode importar variáveis, funções ou classes de outros módulos para usá-los no seu próprio código. */
