1df4cd8629ba94c65d9c892d5214544fb5382987
EvertonRobertoBR <154098335+EvertonRobertoBR@users.noreply.github.com>
2024-06-06 00:07:28 -0300

    javascript

--- BABEL/main.js
+++ BABEL/main.js
@@ -10,7 +10,7 @@ console.log(nobobjt)
 //para converte esse bloco de codigo uso esse comando no TERMINAL:
 
 //npx babel main.js -o bundle.js --presets=@babel/preset-env
-//quando voce executar esse codigo no terminal vai abrir um arquivo chamado bundle.js, dentro dele esta o mesmo codigo mas, aora esta convertido
+//quando voce executar esse codigo no terminal vai abrir um arquivo chamado bundle.js, dentro dele esta o mesmo codigo mas, agora esta convertido
 //esses arquivos pack.json eles foram criados usando o comando --> npm init -y
 
 //apasta node_modules é uma pasta que o babel precisa usar para funcionar corretamente. ela foi criado usando o comando
